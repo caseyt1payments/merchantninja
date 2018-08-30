62aff71395d644537ab72cda6c3d552fb5a2f280
Casey <casey.adcock@t1payments.com>
2018-08-30 13:54:25 -0700

    Initial
    Initial Commit

---
+++ header.php
@@ -0,0 +1,220 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * @package Betheme
+ * @author Muffin group
+ * @link http://muffingroup.com
+ */
+?><!DOCTYPE html>
+<?php
+	if( $_GET && key_exists('mfn-rtl', $_GET) ):
+		echo '<html class="no-js" lang="ar" dir="rtl">';
+	else:
+?>
+<html class="no-js<?php echo mfn_user_os(); ?>" <?php language_attributes(); ?><?php mfn_tag_schema(); ?>>
+<?php endif; ?>
+
+<!-- head -->
+<head>
+<meta property="og:image" content="<?php if(is_front_page()){echo home_url()."/wp-content/uploads/2018/06/toplogo.png";}elseif(has_post_thumbnail()){ echo wp_get_attachment_url( get_post_thumbnail_id(get_the_ID()) );}else{echo home_url()."/wp-content/uploads/2018/06/toplogo.png";} ?>" />
+     <meta property="og:image:type" content="image/jpeg" />
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+<!-- meta -->
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<?php
+	if( mfn_opts_get('responsive') ){
+		if( mfn_opts_get('responsive-zoom') ){
+			echo '<meta name="viewport" content="width=device-width, initial-scale=1" />';
+		} else {
+			echo '<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />';
+		}
+
+	}
+?>
+
+<?php do_action('wp_seo'); ?>
+
+<link rel="shortcut icon" href="<?php mfn_opts_show( 'favicon-img', THEME_URI .'/images/favicon.ico' ); ?>" />
+<?php if( mfn_opts_get('apple-touch-icon') ): ?>
+<link rel="apple-touch-icon" href="<?php mfn_opts_show( 'apple-touch-icon' ); ?>" />
+<?php endif; ?>
+
+<!-- wp_head() -->
+<?php wp_head(); ?>
+
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MBVZ33W');</script>
+<!-- End Google Tag Manager -->
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MBVZ33W"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+
+
+</head>
+
+<!-- body -->
+<body <?php body_class(); ?>>
+
+	<?php do_action( 'mfn_hook_top' ); ?>
+
+	<?php get_template_part( 'includes/header', 'sliding-area' ); ?>
+
+	<?php if( mfn_header_style( true ) == 'header-creative' ) get_template_part( 'includes/header', 'creative' ); ?>
+
+	<!-- #Wrapper -->
+	<div id="Wrapper">
+
+		<?php
+			// Featured Image | Parallax ----------
+			$header_style = '';
+
+			if( mfn_opts_get( 'img-subheader-attachment' ) == 'parallax' ){
+
+				if( mfn_opts_get( 'parallax' ) == 'stellar' ){
+					$header_style = ' class="bg-parallax" data-stellar-background-ratio="0.5"';
+				} else {
+					$header_style = ' class="bg-parallax" data-enllax-ratio="0.3"';
+				}
+
+			}
+		?>
+
+		<?php if( mfn_header_style( true ) == 'header-below' ) echo mfn_slider(); ?>
+
+		<!-- #Header_bg -->
+		<div id="Header_wrapper" <?php echo $header_style; ?>>
+
+			<!-- #Header -->
+			<header id="Header">
+				<?php if( mfn_header_style( true ) != 'header-creative' ) get_template_part( 'includes/header', 'top-area' ); ?>
+				<?php if( mfn_header_style( true ) != 'header-below' ) echo mfn_slider(); ?>
+			</header>
+
+			<?php
+				if( ( mfn_opts_get('subheader') != 'all' ) &&
+					( ! get_post_meta( mfn_ID(), 'mfn-post-hide-title', true ) ) &&
+					( get_post_meta( mfn_ID(), 'mfn-post-template', true ) != 'intro' )	){
+
+
+					$subheader_advanced = mfn_opts_get( 'subheader-advanced' );
+
+					$subheader_style = '';
+
+					if( mfn_opts_get( 'subheader-padding' ) ){
+						$subheader_style .= 'padding:'. mfn_opts_get( 'subheader-padding' ) .';';
+					}
+
+
+					if( is_search() ){
+						// Page title -------------------------
+
+						echo '<div id="Subheader" style="'. $subheader_style .'">';
+							echo '<div class="container">';
+								echo '<div class="column one">';
+
+									if( trim( $_GET['s'] ) ){
+										global $wp_query;
+										$total_results = $wp_query->found_posts;
+									} else {
+										$total_results = 0;
+									}
+
+									$translate['search-results'] = mfn_opts_get('translate') ? mfn_opts_get('translate-search-results','results found for:') : __('results found for:','betheme');
+									echo '<h1 class="title">'. $total_results .' '. $translate['search-results'] .' '. esc_html( $_GET['s'] ) .'</h1>';
+
+								echo '</div>';
+							echo '</div>';
+						echo '</div>';
+
+
+					} elseif( ! mfn_slider_isset() || ( is_array( $subheader_advanced ) && isset( $subheader_advanced['slider-show'] ) ) ){
+						// Page title -------------------------
+
+
+						// Subheader | Options
+						$subheader_options = mfn_opts_get( 'subheader' );
+
+
+						if( is_home() && ! get_option( 'page_for_posts' ) && ! mfn_opts_get( 'blog-page' ) ){
+							$subheader_show = false;
+						} elseif( is_array( $subheader_options ) && isset( $subheader_options[ 'hide-subheader' ] ) ){
+							$subheader_show = false;
+						} elseif( get_post_meta( mfn_ID(), 'mfn-post-hide-title', true ) ){
+							$subheader_show = false;
+						} else {
+							$subheader_show = true;
+						}
+
+
+						// title
+						if( is_array( $subheader_options ) && isset( $subheader_options[ 'hide-title' ] ) ){
+							$title_show = false;
+						} else {
+							$title_show = true;
+						}
+
+
+						// breadcrumbs
+						if( is_array( $subheader_options ) && isset( $subheader_options[ 'hide-breadcrumbs' ] ) ){
+							$breadcrumbs_show = false;
+						} else {
+							$breadcrumbs_show = true;
+						}
+
+						if( is_array( $subheader_advanced ) && isset( $subheader_advanced[ 'breadcrumbs-link' ] ) ){
+							$breadcrumbs_link = 'has-link';
+						} else {
+							$breadcrumbs_link = 'no-link';
+						}
+
+
+						// Subheader | Print
+						if( $subheader_show ){
+							echo '<div id="Subheader" style="'. $subheader_style .'">';
+								echo '<div class="container">';
+									echo '<div class="column one">';
+
+										// Title
+										if( $title_show ){
+											$title_tag = mfn_opts_get( 'subheader-title-tag', 'h1' );
+											echo '<'. $title_tag .' class="title m">'. mfn_page_title() .'</'. $title_tag .'>';
+											echo '<h3>'.get_field('subheader_description').'</h3>';
+										}
+
+										// Breadcrumbs
+										if( $breadcrumbs_show ){
+											mfn_breadcrumbs( $breadcrumbs_link );
+										}
+
+									echo '</div>';
+								echo '</div>';
+							echo '</div>';
+						}
+
+					}
+
+
+				}
+			?>
+
+		</div>
+
+		<?php
+			// Single Post | Template: Intro
+			if( get_post_meta( mfn_ID(), 'mfn-post-template', true ) == 'intro' ){
+				get_template_part( 'includes/header', 'single-intro' );
+			}
+		?>
+
+		<?php do_action( 'mfn_hook_content_before' );
+
+// Omit Closing PHP Tags
